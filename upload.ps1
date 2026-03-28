1fcb68adafc9fa4df149a4e54dd0eadb0a3f58b6
samerhassan95 <arch.samerhassan@gmail.com>
2026-03-28 23:26:11 +0300

    Auto-deploy updates

---
+++ upload.ps1
@@ -0,0 +1,6 @@
+git add .
+git commit -m "Auto-deploy updates"
+git push origin main
+
+echo "🚀 Connecting to server for deployment..."
+ssh samerhassan@8.213.81.38 "cd /home/nomufood/htdocs/nomufood.com && git pull origin main && ./deploy.sh"
